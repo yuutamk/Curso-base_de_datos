9729ac8dd39669b5d24e7621707dbb2b98714974
yuutamk <yuutamk@vk.com>
2025-05-05 08:28:54 -0600

    create views

--- BlogpostDB/01_DDL Create.sql
+++ BlogpostDB/01_DDL Create.sql
@@ -10,7 +10,33 @@ CREATE TABLE people (
 	city varchar(255) 
 );
 
+INSERT INTO `test_db`.`people` (`person_id`, `last_name`, `first_name`, `address`, `city`) 
+VALUES ('1', 'Vásquez', 'Israel', 'Calle Famosa Num 1', 'México'),
+	   ('2', 'Hernández', 'Mónica', 'Reforma 222', 'México'),
+	   ('3', 'Alanis', 'Edgar', 'Central 1', 'Monterrey');
+
+/* Create customers table */
+
+CREATE TABLE customers (
+	customer_id int,
+	customer_name varchar(255),
+	contact_name varchar(255),
+	country varchar(255)
+);
+
+INSERT INTO `test_db`.`customers` (`customer_id`, `customer_name`, `contact_name`, `country`) 
+VALUES (1, 'Customer A', 'Contact A', 'Brasil'),
+	   (2, 'Customer B', 'Contact B', 'Argentina'),
+	   (3, 'Customer C', 'Contact C', 'Brasil');
+
+/* Create view customers */
+
 CREATE VIEW v_brasil_customers AS
-    SELECT customer_name, contact_name
-    FROM customers
-    WHERE country = "Brasil";
+	SELECT customer_name, contact_name
+	FROM customers
+	WHERE country = 'Brasil';
+
+/* Create view people */
+CREATE VIEW v_people AS
+	SELECT person_id, last_name, first_name, address, city
+	FROM people;
